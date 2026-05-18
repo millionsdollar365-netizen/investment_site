850f58def67e645e27d21cd010ce128ec8840011
millionsdollar365-netizen <millionsdollar365@gmail.com>
2026-05-18 16:16:41 -0700

    Fix migration column name: setting_description -> description

--- database/migrations/003_add_crypto_wallets.sql
+++ database/migrations/003_add_crypto_wallets.sql
@@ -2,7 +2,7 @@
 -- Run this migration to support BTC, USDT, and Ethereum wallet configuration
 
 -- Insert wallet settings into settings table (if they don't exist)
-INSERT INTO settings (setting_key, setting_value, setting_description) VALUES
+INSERT INTO settings (setting_key, setting_value, description) VALUES
     ('wallet_btc', '', 'Bitcoin wallet address for deposits'),
     ('wallet_usdt', '', 'USDT wallet address for deposits'),
     ('wallet_ethereum', '', 'Ethereum wallet address for deposits')
